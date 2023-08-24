df826a70d71a39f3b1b30b6c37712c27d4ba7bac
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-08-24 12:07:34 +0200

    ✅ update task.ejs

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const express = require ('express');
 //impot json-server
 const jsonServer = require('json-server');
 
-//appeller le server
-const app = express();
-
 //la fonction listen va faire démarrer le serveur sur le port 3000 
 app.listen(3000, () => console.log('Le serveur est lancé sur le port 3000'));
 
@@ -15,5 +12,30 @@ const fs = require ('fs');
 
 //permet de créer les pages dynamiques
 const bodyParser = require ('body-parser');
+
+//ajouter une route
+app.get('/', (req, res) =>{
+    res.send('Coucou');
+});
+
+//---VARIABLES-----
+
+//middlewhare quand une requete doit s'éxecuter et où
+const jsm = jsonServer.router('db.json');
+
+//appeller le server
+const app = express();
+
+//-----app USE and SET
+
+//pour ne pas convertir en objet
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//dire ce qu'on va traiter c'est en Json
+app.use(bodyParser.json());
+
+//lors qu'on va être sur l'appli elle seront traitées sur le middleware
+
+app.use('/api, jsm');
 
 //
